bd0704bd7e082075cdd2988e2bc453f6cd8856fa
leonardovrodriguez <leovrodriguez03@gmail.com>
2022-11-17 14:49:33 -0500

    edit deploy files

--- ToDo/vite.config.js
+++ ToDo/vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:  process.env.NODE_ENV === 'production'
+          ? '/classProject' // prod
+          : '/', // dev
   plugins: [vue()],
   resolve: {
     alias: {
